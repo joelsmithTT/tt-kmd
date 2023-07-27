364bd906bac6cf791506cef39ddb44113db3d1a5
Andrew Lewycky <alewycky@tenstorrent.com>
2023-07-27 11:22:54 -0400

    Get DMA address limit from device class

--- wormhole.c
+++ wormhole.c
@@ -122,6 +122,7 @@ static void wormhole_reboot(struct tenstorrent_device *tt_dev) {
 struct tenstorrent_device_class wormhole_class = {
 	.name = "Wormhole",
 	.instance_size = sizeof(struct wormhole_device),
+	.dma_address_bits = 35, // NOC->PCIE is offset by 0x8'0000'0000
 	.init_device = wormhole_init,
 	.init_hardware = wormhole_init_hardware,
 	.cleanup_device = wormhole_cleanup,
